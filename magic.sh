504ce34a7fe05d601c906695ced556dcdb3254a8
Morten Olsen <morten@olsen.pro>
2018-03-03 11:58:00 +0100

    .

--- magic.sh
+++ magic.sh
@@ -1,5 +1,14 @@
-INITIALPATH=$PWD
-INSTALLPATH="./env-configs/zsh"
+INSTALLPATH="~/env-configs/zsh"
+
+if [ -d ~/env-configs/zsh ]; then
+  read -p "zsh has already been checked out at the location, do you want to remove it? [y/N]" yn
+  case $yn in
+    [Yy]* );;
+    * ) echo "exiting"; exit 1;;
+  esac
+fi
+
+rm -rf $INSTALLPATH
 echo "Fetching repo"
 mkdir -p $INSTALLPATH
 git clone https://github.com/morten-olsen-env/zsh-config $INSTALLPATH

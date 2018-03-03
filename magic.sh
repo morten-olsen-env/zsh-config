045aa91b216b87e88e5cdaf3366ebaee5b6c1ce7
Morten Olsen <morten@olsen.pro>
2018-03-03 11:52:31 +0100

    .

--- magic.sh
+++ magic.sh
@@ -1,5 +1,8 @@
 INITIALPATH=$PWD
 INSTALLPATH="./env-configs/zsh"
+echo "Fetching repo"
 mkdir -p $INSTALLPATH
 git clone https://github.com/morten-olsen-env/zsh-config $INSTALLPATH
+
+echo "Installing"
 sh $INSTALLPATH/install.sh
